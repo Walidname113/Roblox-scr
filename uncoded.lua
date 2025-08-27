37e929ae5aeec2a0884778db59c64923666f9997
Kiyatsuka <100084394+Walidname113@users.noreply.github.com>
2025-08-27 21:13:26 +0300

    Update uncoded.lua

--- uncoded.lua
+++ uncoded.lua
@@ -215,7 +215,7 @@ function module.CreateUI(title)
 
     local isScaledDown = false
     local normalSize = UDim2.new(0, 600, 0, 400)
-    local smallSize = UDim2.new(0, 400, 0, 400)
+    local smallSize = UDim2.new(0, 450, 0, 400)
 
     trackConnection(scaleButton.MouseButton1Click:Connect(function()
         isScaledDown = not isScaledDown
